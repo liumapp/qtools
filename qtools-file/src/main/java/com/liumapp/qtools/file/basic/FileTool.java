e49d4547ee77dec40cb54f7859e14f34ba303311
liumapp <liumapp.com@gmail.com>
2018-08-07 16:02:58 +0800

    add basic file tool

---
+++ qtools-file/src/main/java/com/liumapp/qtools/file/basic/FileTool.java
@@ -0,0 +1,21 @@
+package com.liumapp.qtools.file.basic;
+
+/**
+ * @author liumapp
+ * @file FileTool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/7
+ */
+public final class FileTool {
+
+    /**
+     * all static method , plz do not initialize the class
+     */
+    private FileTool() {
+        throw new UnsupportedOperationException("not allowed to initialize");
+    }
+
+
+
+}
