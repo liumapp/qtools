f67eec4118d36a51e455a56bd15aafcf9d587c97
liumapp <liumapp.com@gmail.com>
2018-08-07 16:37:04 +0800

    ready to add base64 file tool

---
+++ qtools-file/src/main/java/com/liumapp/qtools/file/base64/Base64FileTool.java
@@ -0,0 +1,21 @@
+package com.liumapp.qtools.file.base64;
+
+/**
+ * @author liumapp
+ * @file Base64FileTool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/7
+ */
+public final class Base64FileTool {
+
+    /**
+     * all static method , plz do not initialize the class
+     */
+    private Base64FileTool() {
+        throw new UnsupportedOperationException("not allowed to initialize");
+    }
+
+    
+
+}
