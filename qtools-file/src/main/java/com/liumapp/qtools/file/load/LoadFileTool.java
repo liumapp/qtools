0ef63cc09207254a0fbbb14c86f218970ad214a2
liumapp <liumapp.com@gmail.com>
2018-08-08 09:13:39 +0800

    make load file by bytes done

--- qtools-file/src/main/java/com/liumapp/qtools/file/load/LoadFileTool.java
+++ qtools-file/src/main/java/com/liumapp/qtools/file/load/LoadFileTool.java
@@ -1,5 +1,10 @@
 package com.liumapp.qtools.file.load;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @author liumapp
  * @file LoadFileTool.java
@@ -16,6 +21,33 @@ public final class LoadFileTool {
         throw new UnsupportedOperationException("not allowed to initialize");
     }
 
-    
+    /**
+     * return byte arrays according to a file object
+     * @param file File object
+     * @return byte[]
+     */
+    public static byte[] loadFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+
+        long length = file.length();
+        if (length > Integer.MAX_VALUE) {
+            // File is too large
+        }
+        byte[] bytes = new byte[(int)length];
+
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+            offset += numRead;
+        }
+
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file "+file.getName());
+        }
+
+        is.close();
+        return bytes;
+    }
 
 }
