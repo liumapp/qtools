42221110ad5bdf182198b0aef5035d5800bb4d8b
liumapp <liumapp.com@gmail.com>
2018-08-08 16:03:52 +0800

    add basic str tool

---
+++ qtools-str/src/main/java/com/liumapp/qtools/str/basic/StrTool.java
@@ -0,0 +1,18 @@
+package com.liumapp.qtools.str.basic;
+
+/**
+ * @author liumapp
+ * @file StrTool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/8
+ */
+public class StrTool {
+
+    private StrTool () {
+        throw new UnsupportedOperationException("not allowed to initialize");
+    }
+
+
+
+}
