4a20592db8dc8ee892fd301012af93baa9b46e3f
liumapp <liumapp.com@gmail.com>
2018-08-08 09:11:26 +0800

    add load file tool

---
+++ qtools-file/src/main/java/com/liumapp/qtools/file/load/LoadFileTool.java
@@ -0,0 +1,21 @@
+package com.liumapp.qtools.file.load;
+
+/**
+ * @author liumapp
+ * @file LoadFileTool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/8
+ */
+public final class LoadFileTool {
+
+    /**
+     * all static method , plz do not initialize the class
+     */
+    private LoadFileTool() {
+        throw new UnsupportedOperationException("not allowed to initialize");
+    }
+
+    
+
+}
