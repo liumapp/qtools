f0308e35522d8dc3e7bfde8c7f78bd17d85a8ee8
liumapp <liumapp.com@gmail.com>
2018-08-08 15:12:16 +0800

    update file tool test

--- qtools-file/src/test/java/com/liumapp/qtools/file/basic/FileToolTest.java
+++ qtools-file/src/test/java/com/liumapp/qtools/file/basic/FileToolTest.java
@@ -2,6 +2,9 @@ package com.liumapp.qtools.file.basic;
 
 import junit.framework.TestCase;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * @author liumapp
  * @file FileToolTest.java
@@ -10,4 +13,12 @@ import junit.framework.TestCase;
  * @date 2018/8/8
  */
 public class FileToolTest extends TestCase {
+
+    private String savePath = "/usr/local/tomcat/project/qtools/data/";
+
+    public void testWriteStringToFile () throws IOException {
+        String str = "sdfsdffqwfasdfawefsadfsdf{dfefd}";
+        FileTool.writeStringToNewFile(new File(savePath + "test.txt"), str);
+    }
+
 }
