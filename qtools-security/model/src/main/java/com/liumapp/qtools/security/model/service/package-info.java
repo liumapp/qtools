640bdc516ed651512d6e3bdccbab2f57e821b158
liumapp <liumapp.com@gmail.com>
2018-08-11 10:06:04 +0800

    rm unnecessary things from qtools-security

--- qtools-security/model/src/main/java/com/liumapp/qtools/security/model/service/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @file package-info.java
- * @author liumapp
- * @email liumapp.com@gmail.com
- * @homepage http://www.liumapp.com
- * @date 3/22/18
- */
-package com.liumapp.qtools.security.model.service;
