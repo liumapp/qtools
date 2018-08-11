4581f5567625adfcf97b821a4e1715095af5e2cc
liumapp <liumapp.com@gmail.com>
2018-08-11 10:04:28 +0800

    add qtools-security

---
+++ qtools-security/model/src/main/java/com/liumapp/qtools/security/model/service/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @file package-info.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 3/22/18
+ */
+package com.liumapp.qtools.security.model.service.impl;
---
+++ qtools-security/tool-a/src/main/java/com/liumapp/qtools/security/toola/ToolAConfig.java
@@ -0,0 +1,17 @@
+package com.liumapp.qtools.security.toola;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+/**
+ * @author liumapp
+ * @file ToolAConfig.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 3/22/18
+ */
+@Configuration
+@Import({OssUtil.class})
+public class ToolAConfig {
+
+
+}
