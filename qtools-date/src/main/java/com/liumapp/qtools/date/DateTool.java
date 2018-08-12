8a6918a5927d8ce1189361fa41e4bda0f430ba16
liumapp <liumapp.com@gmail.com>
2018-08-12 16:46:22 +0800

    add get end date by year number

---
+++ qtools-date/src/main/java/com/liumapp/qtools/date/DateTool.java
@@ -0,0 +1,29 @@
+package com.liumapp.qtools.date;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author liumapp
+ * @file DateTool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/12
+ */
+public class DateTool {
+
+    /**
+     * return a new date according to year number
+     * @param year year number
+     * @return end date
+     */
+    public static Date getEndDateByYearNumber(int year) {
+        Calendar calendar = Calendar.getInstance();
+        Date date = new Date(System.currentTimeMillis());
+        calendar.setTime(date);
+        calendar.add(Calendar.YEAR, year);
+        date = calendar.getTime();
+        return date;
+    }
+
+}
