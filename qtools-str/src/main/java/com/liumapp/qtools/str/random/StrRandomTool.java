17318ce0faa01d4b3fdfc7314bda8a1e0a725083
liumapp <liumapp.com@gmail.com>
2018-08-13 16:54:35 +0800

    add str random tool

---
+++ qtools-str/src/main/java/com/liumapp/qtools/str/random/StrRandomTool.java
@@ -0,0 +1,18 @@
+package com.liumapp.qtools.str.random;
+
+/**
+ * @author liumapp
+ * @file StrRandomTool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/13
+ */
+public final class StrRandomTool {
+
+    private StrRandomTool () {
+        throw new UnsupportedOperationException("not allowed to initialize");
+    }
+
+
+
+}
