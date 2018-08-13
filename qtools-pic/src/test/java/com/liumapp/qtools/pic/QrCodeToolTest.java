ffec741f34bf7670855ba8255ddf4da67331af76
liumapp <liumapp.com@gmail.com>
2018-08-13 16:17:27 +0800

    add qrcode tool test

---
+++ qtools-pic/src/test/java/com/liumapp/qtools/pic/QrCodeToolTest.java
@@ -0,0 +1,16 @@
+package com.liumapp.qtools.pic;
+
+import junit.framework.TestCase;
+
+/**
+ * @author liumapp
+ * @file QrCodeToolTest.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/13
+ */
+public class QrCodeToolTest extends TestCase {
+
+
+
+}
