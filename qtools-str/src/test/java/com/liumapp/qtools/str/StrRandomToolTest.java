b0ba4dce35d96265e812ef59c5265f48bfe89f5a
liumapp <liumapp.com@gmail.com>
2018-08-15 08:53:55 +0800

    add str random tool junit test

--- qtools-str/src/test/java/com/liumapp/qtools/str/StrRandomToolTest.java
+++ qtools-str/src/test/java/com/liumapp/qtools/str/StrRandomToolTest.java
@@ -1,5 +1,8 @@
 package com.liumapp.qtools.str;
 
+import com.liumapp.qtools.str.random.StrRandomTool;
+import junit.framework.TestCase;
+
 /**
  * @author liumapp
  * @file StrRandomToolTest.java
@@ -7,5 +10,11 @@ package com.liumapp.qtools.str;
  * @homepage http://www.liumapp.com
  * @date 2018/8/15
  */
-public class StrRandomToolTest {
+public class StrRandomToolTest extends TestCase {
+
+    public void testGetUUid () {
+        System.out.println(StrRandomTool.getUuid());
+        System.out.println(StrRandomTool.getUuid(true));
+    }
+
 }
