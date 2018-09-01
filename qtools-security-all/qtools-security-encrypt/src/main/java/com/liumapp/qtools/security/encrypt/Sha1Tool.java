35e9c5e01ee2ff28d0b6fd2b0c09a5b1f6cb94fd
liumapp <liumapp.com@gmail.com>
2018-09-01 10:35:45 +0800

    add sha1 tool into security encrypt

---
+++ qtools-security-all/qtools-security-encrypt/src/main/java/com/liumapp/qtools/security/encrypt/Sha1Tool.java
@@ -0,0 +1,16 @@
+package com.liumapp.qtools.security.encrypt;
+
+/**
+ * file Sha1Tool.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/1
+ */
+public class Sha1Tool {
+
+
+
+
+}
