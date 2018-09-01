69decf88ccfda43d1a4ebde080766acc56946612
liumapp <liumapp.com@gmail.com>
2018-09-01 10:47:22 +0800

    add sha1 junit test

---
+++ qtools-security-all/qtools-security-encrypt/src/test/java/com/liumapp/qtools/security/encrypt/Sha1ToolTest.java
@@ -0,0 +1,20 @@
+package com.liumapp.qtools.security.encrypt;
+
+import org.junit.Test;
+
+/**
+ * file Sha1ToolTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/1
+ */
+public class Sha1ToolTest {
+
+    @Test
+    public void testSHA1 () {
+        
+    }
+
+}
