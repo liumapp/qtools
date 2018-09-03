804c7114f7277c245693380579ec363892a03754
liumapp <liumapp.com@gmail.com>
2018-09-03 08:59:49 +0800

    add get utc date method

--- qtools-date/src/main/java/com/liumapp/qtools/date/DateTool.java
+++ qtools-date/src/main/java/com/liumapp/qtools/date/DateTool.java
@@ -1,5 +1,7 @@
 package com.liumapp.qtools.date;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -36,6 +38,37 @@ public class DateTool {
      */
     public static String getSimpleDateString (Date date) {
         return new SimpleDateFormat("yyyyMMddHH").format(date);
+    }
+
+    /**
+     * yyyyMMddHH 2018090108
+     */
+    public static String getSimpleUTCDateString (Date date) {
+        DateFormat format = new SimpleDateFormat("yyyyMMddHH");
+        StringBuffer UTCTimeBuffer = new StringBuffer();
+        // 1、取得本地时间：
+        Calendar cal = Calendar.getInstance() ;
+        // 2、取得时间偏移量：
+        int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);
+        // 3、取得夏令时差：
+        int dstOffset = cal.get(java.util.Calendar.DST_OFFSET);
+        // 4、从本地时间里扣除这些差量，即可以取得UTC时间：
+        cal.add(java.util.Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH)+1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        UTCTimeBuffer.append(year).append("-").append(month).append("-").append(day) ;
+        UTCTimeBuffer.append(" ").append(hour).append(":").append(minute) ;
+        try{
+            format.parse(UTCTimeBuffer.toString()) ;
+            return UTCTimeBuffer.toString() ;
+        }catch(ParseException e)
+        {
+            e.printStackTrace() ;
+        }
+        return null ;
     }
 
 }
