52fc61363f19f51eeb03038e709b0d65c5832c58
liumapp <liumapp.com@gmail.com>
2018-09-08 15:32:34 +0800

    add each service

---
+++ qtools-data/src/main/java/com/liumapp/qtools/data/service/Each.java
@@ -0,0 +1,13 @@
+package com.liumapp.qtools.data.service;
+
+/**
+ * file Each.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/8
+ */
+public interface Each<E> {
+    public void dealEach(E e) throws Exception;
+}
