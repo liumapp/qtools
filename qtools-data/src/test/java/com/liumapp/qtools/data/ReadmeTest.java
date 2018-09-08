084988a5e2fec81df56a388c7ea20024b0d38aea
liumapp <liumapp.com@gmail.com>
2018-09-08 16:03:40 +0800

    ready to add junit test

---
+++ qtools-data/src/test/java/com/liumapp/qtools/data/ReadmeTest.java
@@ -0,0 +1,15 @@
+package com.liumapp.qtools.data;
+
+/**
+ * file ReadmeTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/8
+ */
+public class ReadmeTest {
+
+
+
+}
