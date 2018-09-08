feb8331bc96568fa93a889d789a142fe834ec36d
liumapp <liumapp.com@gmail.com>
2018-09-08 15:33:51 +0800

    add multiple service

---
+++ qtools-data/src/main/java/com/liumapp/qtools/data/service/Multiple.java
@@ -0,0 +1,13 @@
+package com.liumapp.qtools.data.service;
+
+/**
+ * file Multiple.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/8
+ */
+public interface Multiple {
+    public Object[] create() throws Exception;
+}
