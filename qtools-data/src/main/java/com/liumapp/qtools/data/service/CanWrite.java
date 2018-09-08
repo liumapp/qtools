c61f70bc19f035cc34f62fe23436ec6f5034179c
liumapp <liumapp.com@gmail.com>
2018-09-08 15:31:43 +0800

    add can write interface

---
+++ qtools-data/src/main/java/com/liumapp/qtools/data/service/CanWrite.java
@@ -0,0 +1,18 @@
+package com.liumapp.qtools.data.service;
+
+import com.liumapp.qtools.data.core.Row;
+
+/**
+ * file CanWrite.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/8
+ */
+public interface CanWrite {
+
+    public boolean isCanWrite() ;
+
+    public Row setCanWrite(boolean canWrite) ;
+}
