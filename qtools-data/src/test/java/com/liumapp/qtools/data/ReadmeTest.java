80469ba14ec0ae037785de9990a5ea4e46f48b0f
liumapp <liumapp.com@gmail.com>
2018-09-08 16:20:48 +0800

    under coding to finish reading jdbc data and write into csv file

--- qtools-data/src/test/java/com/liumapp/qtools/data/ReadmeTest.java
+++ qtools-data/src/test/java/com/liumapp/qtools/data/ReadmeTest.java
@@ -1,5 +1,20 @@
 package com.liumapp.qtools.data;
 
+import com.liumapp.qtools.data.core.Model;
+import com.liumapp.qtools.data.core.Row;
+import com.liumapp.qtools.data.csv.CSVWriter;
+import com.liumapp.qtools.data.jdbc.JDBCReader;
+import com.liumapp.qtools.data.jdbc.JDBCWriter;
+import com.liumapp.qtools.data.service.Reader;
+import com.liumapp.qtools.data.service.Transformer;
+import com.liumapp.qtools.data.service.Writer;
+import org.apache.commons.csv.CSVFormat;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * file ReadmeTest.java
  * author liumapp
@@ -10,6 +25,46 @@ package com.liumapp.qtools.data;
  */
 public class ReadmeTest {
 
+    private boolean debug = true;
+
+    /**
+     * todo
+     */
+    @Test
+    public void testReaderAndWriteIntoCSVFromJDBC () throws Exception {
+
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/sdk_emergency";
+        String username = "root";
+        String password = "adminadmin";
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(url,username,password);
+
+        Reader reader = new JDBCReader()
+                .setConnection(connection) // jdbc connection
+                .setSQL("select * from emergency_app"); // any sql
+
+        Transformer transformer = new Transformer<Row>() {
+            @Override
+            public void onEvent(Row event, long sequence, boolean endOfBatch) throws Exception {
+                //deal each row here
+                System.out.println("event is :");
+                System.out.println(event.toString());
+                System.out.println("sequence is :");
+                System.out.println(sequence);
+                System.out.println("endofbatch is :");
+                System.out.println(endOfBatch);
+            }
+        };
+
+//        Writer[] writers = new CSVWriter();
+
+        Model model = new Model()
+                .setReader(reader)
+                .setTransformer(transformer);
+
+        model.start();
+    }
 
 
 }
