5eef652e379406e56ac652f3f8eef6dd95e5c498
liumapp <liumapp.com@gmail.com>
2018-09-08 15:31:06 +0800

    add service and row : can read

---
+++ qtools-data/src/main/java/com/liumapp/qtools/data/service/CanRead.java
@@ -0,0 +1,19 @@
+package com.liumapp.qtools.data.service;
+
+import com.liumapp.qtools.data.core.Row;
+
+/**
+ * file CanRead.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/8
+ */
+public interface CanRead {
+
+    public boolean isCanRead() ;
+
+    public Row setCanRead(boolean canRead) ;
+}
+
