72490599a843f472458798844c224626b2b2833b
liumapp <liumapp.com@gmail.com>
2018-09-30 14:08:30 +0800

    add time zone date tool test

--- qtools-date/src/test/java/com/liumapp/qtools/date/TimeZoneDateToolTest.java
+++ qtools-date/src/test/java/com/liumapp/qtools/date/TimeZoneDateToolTest.java
@@ -1,15 +1,15 @@
 package com.liumapp.qtools.date;
 
 /**
- * file TimeZoneDateTool.java
+ * file TimeZoneDateToolTest.java
  * author liumapp
  * github https://github.com/liumapp
  * email liumapp.com@gmail.com
  * homepage http://www.liumapp.com
  * date 2018/9/30
  */
-public class TimeZoneDateTool {
+public class TimeZoneDateToolTest {
 
-    
+
 
 }
