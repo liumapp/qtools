acc65d792ca8fed9f3955c64c84835609cbe9e0d
liumapp <liumapp.com@gmail.com>
2018-09-30 23:15:17 +0800

    add qtools class in all module

---
+++ qtools-all/src/main/java/com/liumapp/qtools/all/Qtools.java
@@ -0,0 +1,16 @@
+package com.liumapp.qtools.all;
+
+/**
+ * todo support java cli command
+ * file Qtools.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/30
+ */
+public class Qtools {
+
+
+
+}
