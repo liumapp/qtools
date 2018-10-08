df3da76ad4bb06e85a37d066103317fb91dd8161
liumapp <liumapp.com@gmail.com>
2018-10-08 11:25:59 +0800

    add main class for qtools spring boot starter

---
+++ qtools-starter-all/qtools-spring-boot-starter/src/main/java/com/liumapp/qtools/starter/springboot/QtoolsSpringBootStarter.java
@@ -0,0 +1,17 @@
+package com.liumapp.qtools.starter.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file QtoolsSpringBootStarter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/10/8
+ */
+@SpringBootApplication
+public class QtoolsSpringBootStarter {
+
+
+}
