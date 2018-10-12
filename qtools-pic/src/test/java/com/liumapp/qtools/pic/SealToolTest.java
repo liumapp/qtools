d71a68e67b2667cc7c460eb00dcae97383a39958
liumapp <liumapp.com@gmail.com>
2018-10-12 10:12:50 +0800

    add scope compile for dependencies

--- qtools-pic/src/test/java/com/liumapp/qtools/pic/SealToolTest.java
+++ qtools-pic/src/test/java/com/liumapp/qtools/pic/SealToolTest.java
@@ -19,7 +19,7 @@ public class SealToolTest extends TestCase {
     private String savePath = "/usr/local/tomcat/project/qtools/data/";
 
     public void testGenerateCompanySearl () throws IOException {
-        SealTool.generateSealFile("广东某某网络技术有限公司", savePath + "test.png");
+        SealTool.generateSealFile("浙江葫芦娃网络集团有限公司", savePath + "test.png");
         Assert.assertEquals(true, FileTool.isFileExists(savePath + "test.png"));
 
         try {
