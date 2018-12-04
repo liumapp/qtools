0f5fd56154a427fbdb305da3f9e41b03cb0e81ab
liumapp <liumapp.com@gmail.com>
2018-12-04 09:52:14 +0800

    add url download tool class

---
+++ qtools-http/src/main/java/com/liumapp/qtools/http/UrlDownloadTool.java
@@ -0,0 +1,15 @@
+package com.liumapp.qtools.http;
+
+/**
+ * file UrlDownloadTool.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/4
+ */
+public class UrlDownloadTool {
+
+
+
+}
