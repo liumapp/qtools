eaacc6f0e1c012540e31eef18a883189dec329b8
liumapp <liumapp.com@gmail.com>
2018-12-06 10:40:10 +0800

    init qtools-property by maven archetype

---
+++ qtools-property/src/main/java/com/liumapp/qtools/property/TestMain.java
@@ -0,0 +1,22 @@
+package com.liumapp.qtools.property;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file TestMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/6
+ */
+@SpringBootApplication
+public class TestMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(TestMain.class, args);
+    }
+
+}
