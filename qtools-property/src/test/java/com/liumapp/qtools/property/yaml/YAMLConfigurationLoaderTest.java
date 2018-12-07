aa238da46a9209dd21a44f1ff419bcff8947ccb8
liumapp <liumapp.com@gmail.com>
2018-12-07 16:44:39 +0800

    add yaml junit test case done

---
+++ qtools-property/src/test/java/com/liumapp/qtools/property/yaml/YAMLConfigurationLoaderTest.java
@@ -0,0 +1,45 @@
+package com.liumapp.qtools.property.yaml;
+
+
+import com.liumapp.qtools.property.core.ConfigurationNode;
+import com.liumapp.qtools.property.core.loader.ConfigurationLoader;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * file YAMLConfigurationLoaderTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/12/7
+ * Basic sanity checks for the loader
+ */
+public class YAMLConfigurationLoaderTest {
+
+    @Test
+    public void testSimpleLoading() throws IOException {
+        URL url = getClass().getResource("/yaml/example.yml");
+        ConfigurationLoader loader = YAMLConfigurationLoader.builder()
+                .setURL(url).build();
+        ConfigurationNode node = loader.load();
+        assertEquals("unicorn", node.getNode("test", "op-level").getValue());
+        assertEquals("dragon", node.getNode("other", "op-level").getValue());
+        assertEquals("dog park", node.getNode("other", "location").getValue());
+
+
+        Function<Object, Map<String, List>> f = o -> (HashMap<String, List>)o;
+        List<Map<String, List>> fooList = new ArrayList<>(node.getNode("foo").getList(f));
+        assertEquals(0, fooList.get(0).get("bar").size());
+    }
+}
