e34fad0492e39bbff3fec71a3731304d54a324fc
liumapp <liumapp.com@gmail.com>
2019-01-18 15:44:38 +0800

    tmp

--- qtools-collection/src/test/java/com/liumapp/qtools/collection/array/ArrayQToolTest.java
+++ qtools-collection/src/test/java/com/liumapp/qtools/collection/array/ArrayQToolTest.java
@@ -1,5 +1,6 @@
 package com.liumapp.qtools.collection.array;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -16,8 +17,22 @@ import static org.mockito.Mockito.*;
 public class ArrayQToolTest {
 
     @Test
-    public void clearArray() {
-        
+    public void clearStringArray() {
+        String[] strings = mock(String[].class);
+        when(strings[0]).thenReturn("test1");
+        when(strings[1]).thenReturn("test2");
+        when(strings[2]).thenReturn(null);
+        when(strings[3]).thenReturn("");
+        when(strings[4]).thenReturn("test3");
+        when(strings[5]).thenReturn(null);
+
+        ArrayQTool arrayQTool = mock(ArrayQTool.class);
+
+//        String[] cleanArray = (String[]) ArrayTool.clearArray(testArray);
+//        Assert.assertEquals("test1", cleanArray[0]);
+//        Assert.assertEquals("test2", cleanArray[1]);
+//        Assert.assertEquals("test3", cleanArray[2]);
+//        Assert.assertEquals(3, cleanArray.length);
     }
 
     @Test
