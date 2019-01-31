530d5523396c2a1f091016efbd51dbe0c68efc88
liumapp <liumapp.com@gmail.com>
2019-01-31 10:15:28 +0800

    ready to test string array tool

--- qtools-collection/src/main/java/com/liumapp/qtools/collection/array/StringArrayTool.java
+++ qtools-collection/src/main/java/com/liumapp/qtools/collection/array/StringArrayTool.java
@@ -9,4 +9,23 @@ package com.liumapp.qtools.collection.array;
  * date 2019/1/31
  */
 public class StringArrayTool {
+
+    /**
+     * Convert String[] to comma separated string
+     * @param stringArray string array ["a", "b" ,"c"]
+     * @return a string with comma 'a','b','c'
+     */
+    public static String StringArrayToCommaLine (String[] stringArray) {
+        if (stringArray.length > 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String item : stringArray) {
+                stringBuilder.append("'").append(item.replace("'", "\\'")).append("',");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            return stringBuilder.toString();
+        } else {
+            return "";
+        }
+    }
+
 }
