8ed0e34233af7c623b38816844ab45ec175b22c9
liumapp <liumapp.com@gmail.com>
2019-02-16 16:32:58 +0800

    add WriteFileTool class

---
+++ qtools-file/src/main/java/com/liumapp/qtools/file/write/WriteFileTool.java
@@ -0,0 +1,15 @@
+package com.liumapp.qtools.file.write;
+
+/**
+ * file WriteFileTool.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/2/16
+ */
+public class WriteFileTool {
+
+
+
+}
