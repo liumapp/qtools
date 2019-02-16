f51c5a41e4ae55dfad5e8da527f25d091b9ff63e
liumapp <liumapp.com@gmail.com>
2019-02-16 22:28:45 +0800

    add big file tool test case class

---
+++ qtools-file/src/test/java/com/liumapp/qtools/file/basic/BigFileToolTest.java
@@ -0,0 +1,17 @@
+package com.liumapp.qtools.file.basic;
+
+import junit.framework.TestCase;
+
+/**
+ * file BigFileToolTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/2/16
+ */
+public class BigFileToolTest extends TestCase {
+
+
+
+}
