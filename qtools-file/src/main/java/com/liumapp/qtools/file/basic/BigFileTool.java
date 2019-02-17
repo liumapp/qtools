99f3dbf423f60574341b407191e1bcb2325902c1
liumapp <liumapp.com@gmail.com>
2019-02-17 10:54:00 +0800

    ready to test create big file with repeat row method

--- qtools-file/src/main/java/com/liumapp/qtools/file/basic/BigFileTool.java
+++ qtools-file/src/main/java/com/liumapp/qtools/file/basic/BigFileTool.java
@@ -1,6 +1,8 @@
 package com.liumapp.qtools.file.basic;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 /**
@@ -13,41 +15,21 @@ import java.util.List;
  */
 public class BigFileTool {
 
-    public static void writeRaw(List<String> records) throws IOException {
-        File file = File.createTempFile("foo", ".txt");
-        try {
-            FileWriter writer = new FileWriter(file);
-            System.out.print("Writing raw... ");
-            write(records, writer);
-        } finally {
-            // comment this out if you want to inspect the files afterward
-            file.delete();
-        }
-    }
+   public static boolean createBigFileWithRepeatRow (String path, String singleLineContent, Long rows) throws IOException {
+       FileOutputStream fosRef = new FileOutputStream(new File(path));
+       FileChannel fileChannel = fosRef.getChannel();
 
-    public static void writeBuffered(List<String> records, int bufSize) throws IOException {
-        File file = File.createTempFile("foo", ".txt");
-        try {
-            FileWriter writer = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(writer, bufSize);
+       ByteBuffer buffer = ByteBuffer.allocate(2048);
+       for (long i = 0 ; i < rows ; i++) {
+            buffer.put(singleLineContent.getBytes());
+            buffer.flip();
+            fileChannel.write(buffer);
+            buffer.clear();
+       }
 
-            System.out.print("Writing buffered (buffer size: " + bufSize + ")... ");
-            write(records, bufferedWriter);
-        } finally {
-            // comment this out if you want to inspect the files afterward
-            file.delete();
-        }
-    }
-
-    public static void write(List<String> records, Writer writer) throws IOException {
-        long start = System.currentTimeMillis();
-        for (String record: records) {
-            writer.write(record);
-        }
-        writer.flush();
-        writer.close();
-        long end = System.currentTimeMillis();
-        System.out.println((end - start) / 1000f + " seconds");
-    }
+       fileChannel.close();
+       fosRef.close();
+       return true;
+   }
 
 }
