6cf50d2f2c8b99f7f4e634114bcd3f043a064652
liumapp <liumapp.com@gmail.com>
2019-02-17 10:31:51 +0800

    create big file done , need update

--- qtools-file/src/test/java/com/liumapp/qtools/file/basic/BigFileToolTest.java
+++ qtools-file/src/test/java/com/liumapp/qtools/file/basic/BigFileToolTest.java
@@ -1,6 +1,15 @@
 package com.liumapp.qtools.file.basic;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * file BigFileToolTest.java
@@ -12,6 +21,18 @@ import junit.framework.TestCase;
  */
 public class BigFileToolTest extends TestCase {
 
+    @Test
+    public void testWriteBigFile () throws IOException {
+        byte[] buffer = "test content for writing long file \n".getBytes();
+        int number_of_lines = 8_000_000;
 
+        FileChannel rwChannel = new RandomAccessFile("testfile.txt", "rw").getChannel();
+        ByteBuffer wrBuf = rwChannel.map(FileChannel.MapMode.READ_WRITE, 0, buffer.length * number_of_lines);
+        for (int i = 0; i < number_of_lines; i++)
+        {
+            wrBuf.put(buffer);
+        }
+        rwChannel.close();
+    }
 
 }
