9ec89c8c87fe1234e2b8d360a3042ebee398faf0
liumapp <liumapp.com@gmail.com>
2019-03-07 14:26:16 +0800

    add zip tool test class

---
+++ qtools-collection/src/test/java/com/liumapp/qtools/collection/zip/ZipToolTest.java
@@ -0,0 +1,42 @@
+package com.liumapp.qtools.collection.zip;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * file ZipToolTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/3/7
+ */
+public class ZipToolTest {
+
+    @Test
+    public void zip() {
+
+    }
+
+    @Test
+    public void zip1() {
+    }
+
+    @Test
+    public void zip2() {
+    }
+
+    @Test
+    public void unzip() {
+    }
+
+    @Test
+    public void unzip1() {
+    }
+
+    @Test
+    public void unzip2() {
+    }
+
+}
