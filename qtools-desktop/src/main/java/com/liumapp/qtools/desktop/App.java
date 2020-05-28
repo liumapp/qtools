1eb9e8c7143478f71d0be0e6f27630e1e335124e
liumapp <liumapp.com@gmail.com>
2020-05-28 13:58:34 +0800

    create qtools-desktop module

---
+++ qtools-desktop/src/main/java/com/liumapp/qtools/desktop/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.qtools.desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
