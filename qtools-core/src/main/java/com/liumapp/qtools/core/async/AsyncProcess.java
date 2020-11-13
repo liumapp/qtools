016af63c790a6fead9b96c78236ea04213bbed94
liumapp <liumapp.com@gmail.com>
2020-11-13 09:49:12 +0800

    create async process

---
+++ qtools-core/src/main/java/com/liumapp/qtools/core/async/AsyncProcess.java
@@ -0,0 +1,15 @@
+package com.liumapp.qtools.core.async;
+
+/**
+ * file AsyncProcess.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2020/11/13
+ */
+public class AsyncProcess {
+
+
+
+}
