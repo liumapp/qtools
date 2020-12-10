b01e5525570227900ac942ab24d290d30d4250c1
liumapp <liumapp.com@gmail.com>
2020-12-10 17:19:18 +0800

    add qtools test case class

---
+++ src/test/java/com/liumapp/qtools/QtoolsTest.java
@@ -0,0 +1,19 @@
+package com.liumapp.qtools;
+
+import junit.framework.TestCase;
+
+/**
+ * file QtoolsTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2020/12/10
+ */
+public class QtoolsTest extends TestCase {
+
+    public void testBase64() {
+
+    }
+
+}
