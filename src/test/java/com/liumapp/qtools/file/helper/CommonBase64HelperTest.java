1fd1b874f48227646bca96450b207a3c0b6b2dba
liumapp <liumapp.com@gmail.com>
2020-12-12 11:18:33 +0800

    create common base64 helper test case

---
+++ src/test/java/com/liumapp/qtools/file/helper/CommonBase64HelperTest.java
@@ -0,0 +1,28 @@
+package com.liumapp.qtools.file.helper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * file CommonBase64HelperTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2020/12/12
+ */
+public class CommonBase64HelperTest {
+
+    @Test
+    public void stringToBytes() {
+    }
+
+    @Test
+    public void header() {
+    }
+
+    @Test
+    public void removeHeader() {
+    }
+}
