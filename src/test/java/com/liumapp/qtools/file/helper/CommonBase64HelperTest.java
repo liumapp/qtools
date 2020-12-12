fb2b4742f3124309acba83ecbfb64e7a1adf8b54
liumapp <liumapp.com@gmail.com>
2020-12-12 12:06:13 +0800

    update common base64 helper test case

--- src/test/java/com/liumapp/qtools/file/helper/CommonBase64HelperTest.java
+++ src/test/java/com/liumapp/qtools/file/helper/CommonBase64HelperTest.java
@@ -1,5 +1,9 @@
 package com.liumapp.qtools.file.helper;
 
+import com.liumapp.qtools.Qtools;
+import com.liumapp.qtools.QtoolsFactory;
+import com.liumapp.qtools.file.core.FileHelper;
+import com.liumapp.qtools.file.core.enums.IOEnum;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,15 +18,26 @@ import static org.junit.Assert.*;
  */
 public class CommonBase64HelperTest {
 
+    private Qtools qtools = QtoolsFactory.getFactoryInstance().getInstance();
+
+    private FileHelper fileHelper = qtools.newFileHelperBuilder()
+            .setIoType(IOEnum.NIO)
+            .setSupportTransferTo(true)
+            .setAutoCreateFolder(true)
+            .build();
+
     @Test
     public void stringToBytes() {
     }
 
     @Test
     public void header() {
+
     }
 
     @Test
     public void removeHeader() {
+
     }
+
 }
