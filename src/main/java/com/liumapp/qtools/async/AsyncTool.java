e758c11301c7b365b12fe84dfdf1edb21ea1e654
liumapp <liumapp.com@gmail.com>
2021-03-02 10:16:47 +0800

    create async tool sbi

---
+++ src/main/java/com/liumapp/qtools/async/AsyncTool.java
@@ -0,0 +1,17 @@
+package com.liumapp.qtools.async;
+
+import com.liumapp.qtools.core.annotations.SPI;
+
+/**
+ * @file AsyncTool.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2021/3/2 09:46
+ */
+@SPI
+public interface AsyncTool {
+
+    public String sayHello ();
+
+}
---
+++ src/main/java/com/liumapp/qtools/async/FutureAsyncTool.java
@@ -0,0 +1,17 @@
+package com.liumapp.qtools.async;
+
+/**
+ * @file FutureAsyncTool.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2021/3/2 10:16
+ */
+public class FutureAsyncTool implements AsyncTool {
+
+    @Override
+    public String sayHello() {
+        return "from future async tool";
+    }
+
+}
