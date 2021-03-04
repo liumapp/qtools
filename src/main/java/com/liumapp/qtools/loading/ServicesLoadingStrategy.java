f614fb8d5657049e6be63d4f6e3cfc679496175b
liumapp <liumapp.com@gmail.com>
2021-03-04 09:27:50 +0800

    create services loading strategy clz

---
+++ src/main/java/com/liumapp/qtools/loading/ServicesLoadingStrategy.java
@@ -0,0 +1,26 @@
+package com.liumapp.qtools.loading;
+
+/**
+ * @file ServicesLoadingStrategy.java
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2021/3/4 09:26
+ */
+public class ServicesLoadingStrategy implements LoadingStrategy {
+
+    @Override
+    public String directory() {
+        return "META-INF/services/";
+    }
+
+    @Override
+    public boolean overridden() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return MIN_PRIORITY;
+    }
+}
